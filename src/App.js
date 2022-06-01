16a8deed77172fa8368808d35f989b5f8bd9927e
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-06-01 16:23:10 +0300

    Fix function component to class component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return <Calculator />;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <Calculator />;
+  }
 }
 
 export default App;
